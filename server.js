38d5498c6d00576035ed9643a2fa02960ac49f74
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2025-08-11 23:30:21 +0530

    testing giftygen

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ const server = app.listen(process.env.PORT, () => {
 process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
   console.log(`Shutting down the server due to Unhandled Promise Rejection`);
-
   server.close(() => {
     process.exit(1);
   });
